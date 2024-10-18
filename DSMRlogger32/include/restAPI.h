907b11df30c8efb4feb91c6139d4673c84dd300a
Willem Aandewiel <Willem@Aandewiel.nl>
2024-10-18 10:17:51 +0200

    Shield API's

--- DSMRlogger32/include/restAPI.h
+++ DSMRlogger32/include/restAPI.h
@@ -14,13 +14,15 @@ extern time_t   now;
 //============ Prototypes ==================
 void processApiV2Sm(const char* apiId, const char* oneField);
 void processApiV2Dev(const char *URI, const char *apiId, const char *word5, const char *word6);
+void processApiV2Shield(const char *URI, const char *apiId, const char *word5, const char *word6);
 void processApiV2Hist(const char *URI, const char *apiId, const char *word5, const char *word6);
-void sendDeviceInfo();                                      
+void sendDeviceInfo();
 void sendDeviceTime();                                      
 void sendDevSettings();                                     
 void sendSMsettings();   
 void sendShieldSettings();   
 void sendDeviceDebug(const char *URI, String tail);         
+void sendShieldInfo();
 void sendJsonV2smApi(const char *firstLevel);               
 void sendJsonActualHist();                                  
 void sendJsonHist(int8_t ringType, const char *fileName, timeStruct useTime, uint8_t limit, bool sortDesc);
